eacab52e3dd1114aef2f839b76e77ab9298fc604
Adam Garstka <adam.garstka@netguru.co>
2015-11-30 10:57:42 +0100

    Add contact person association to check assignment model

--- app/models/check_assignment.rb
+++ app/models/check_assignment.rb
@@ -1,6 +1,7 @@
 class CheckAssignment < ActiveRecord::Base
   belongs_to :project_check
   belongs_to :user
+  belongs_to :contact_person, class_name: "User"
 
   validates :user, presence: true
 end

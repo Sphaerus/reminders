588075a4bf72072a2e7398d6219d0f0f4381bbea
Adam Garstka <adam.garstka@netguru.co>
2015-12-01 14:12:23 +0100

    Create Projects::Create use case

---
+++ spec/use_cases/projects/create_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe Projects::Create do
+  let(:attrs) { attributes_for(:project) }
+  let(:projects_repo) { ProjectsRepository.new }
+  let(:service) { described_class.new(attrs: attrs, projects_repo: projects_repo) }
+
+  describe "#call" do
+    context "with valid params" do
+      it "creates the object" do
+        expect { service.call }.to change { projects_repo.all.count }.by(1)
+      end
+    end
+
+    context "with invalid params" do
+      let(:attrs) { { name: "" }  }
+
+      it "doesn't create object" do
+        expect { service.call }.to_not change { projects_repo.all.count }
+      end
+    end
+  end
+end

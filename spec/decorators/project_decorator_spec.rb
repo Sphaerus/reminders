f114d2298616d08394bc335ab9960d0826f863a9
Szymon Baranowski <szymon.baranowski@netguru.pl>
2016-02-15 13:18:21 +0100

    Add new filter options and tabs order for projects

--- spec/decorators/project_decorator_spec.rb
+++ spec/decorators/project_decorator_spec.rb
@@ -8,11 +8,13 @@ describe ProjectDecorator do
     context "project is archived" do
       before { project.archived_at = Time.current }
       it { expect(decorator.archive_button_class).to eq("disabled") }
+      it { expect(decorator.status_text).to eq("archived") }
     end
 
     context "project is not archived" do
       before { project.archived_at = nil }
       it { expect(decorator.archive_button_class).to be_nil }
+      it { expect(decorator.status_text).to eq("active") }
     end
   end
 end

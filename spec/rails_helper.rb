dbcb1a96299b6f3ffef5334a38f4623d686c5624
Szymon Baranowski <szymon.baranowski@netguru.pl>
2016-02-16 12:31:41 +0100

    Change javascript driver to selenium

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,7 +26,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
-  Capybara.javascript_driver = :webkit
+  Capybara.javascript_driver = :selenium
   config.include RSpecHtmlMatchers
   config.include FeatureHelpers, type: :feature
 

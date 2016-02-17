f55b4627c542e8fafd4fc7ff18632409d1eb6ad1
Szymon Baranowski <szymon.baranowski@netguru.pl>
2016-02-17 12:03:10 +0100

    Add more jQuery functions to execute actions

--- spec/support/pages/reminders/sections/project_checks_table_row.rb
+++ spec/support/pages/reminders/sections/project_checks_table_row.rb
@@ -2,29 +2,12 @@ require_relative "../sections/project_checks_history_table.rb"
 
 module Reminders
   class ProjectChecksTableRow < SitePrism::Section
-    element :project, "td:first"
-    element :last_check_date, "td[2]"
-    element :last_checker, "td[4]"
-    element :history_button, "td[5]"
-    element :check_button, "td[6] button"
-    element :toggle_state_button, ".toggle-state a"
-    element :assigned_reviewer, ".assigned-person"
-    element :pick_random_button, ".pick-random-button"
-    element :reassign_random_button, ".reassign-random-button"
-    section :history_table, ProjectChecksHistoryTable,
-            :xpath, "ancestor::tbody/following-sibling::tbody",
-            match: :first
-
     def id
       root_element[:id]
     end
 
     def disabled?
-      toggle_state_button[:class] == "btn btn-success"
-    end
-
-    def toggle_state
-      toggle_state_button.click
+      evaluate_script("$('.toggle-switch').attr('checked')").nil?
     end
   end
 end

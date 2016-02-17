ce5e2f7a8d8a8b344cdec2bcfe69dcdb43838c08
Szymon Baranowski <szymon.baranowski@netguru.pl>
2016-02-17 12:45:21 +0100

    Fix pausing_users_spec.rb

--- spec/features/pausing_users_spec.rb
+++ spec/features/pausing_users_spec.rb
@@ -6,27 +6,21 @@ feature "pausing users" do
            uid: "12331", provider: "google_oauth2",
            email: "john@doe.pl", admin: true, paused: false)
   end
-  let(:second_user) do
-    create(:user,
-           name: "John Smith",
-           email: "john@smith@foo.pl",
-           paused: true)
-  end
   let(:users_page) { Users::UsersPage.new }
 
   before do
     log_in(user)
-    second_user.touch
   end
 
-  pending "toggling state of paused in '/users' page" do
+  scenario "toggling state of paused in '/users' page", js: true do
     users_page.load
-    expect(users_page.user_rows.first.paused?).to be_falsy
-    expect(users_page.user_rows.second.paused?).to be_truthy
+    expect(users_page.user_rows.first.paused?).to be false
     users_page.user_rows.first.pause!
-    expect(users_page.user_rows.first.paused?).to be_truthy
-    expect(user.reload.paused).to be_truthy
+    wait_for_jquery
+    expect(users_page.user_rows.first.paused?).to be true
+    expect(user.reload.paused).to be true
     users_page.user_rows.first.unpause!
-    expect(users_page.user_rows.first.paused?).to be_falsy
+    wait_for_jquery
+    expect(users_page.user_rows.first.paused?).to be false
   end
 end

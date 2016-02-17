2fda310edde949f88d93a2aba11528f411e189ba
Szymon Baranowski <szymon.baranowski@netguru.pl>
2016-02-17 13:56:04 +0100

    Fix project_check_history_spec.rb

--- spec/features/project_check_history_spec.rb
+++ spec/features/project_check_history_spec.rb
@@ -11,46 +11,22 @@ feature "project checks history" do
   end
 
   let(:user) { create(:user, uid: "213312", provider: "google_oauth2") }
-  let(:page) { Reminders::ReminderPage.new }
-  let(:second_user) { create(:user, name: "Sam Smith") }
+  let(:reminders_page) { Reminders::ReminderPage.new }
 
   before do
     log_in(user)
   end
 
-  pending "history shows only done project checks" do
-    page.load reminder_id: reminder.id
+  scenario "history shows done checks", js: true do
+    reminders_page.load reminder_id: reminder.id
 
-    expect(page.first_project.last_checker)
-      .not_to have_text user.name
+    expect(page).not_to have_content user.name
+    page.find("button", text: "I've done this!").click
+    page.driver.browser.switch_to.alert.accept
 
-    page.first_project.check_button.click
+    expect(page).to have_content user.name
 
-    expect(page.first_project.last_checker)
-      .to have_text user.name
-    expect(page.first_project.assigned_reviewer)
-      .not_to have_text second_user.name
-
-    page.first_project.history_button.click
-
-    expect(page.first_project.history_table.content)
-      .to have_text user.name
-
-    page.first_project.pick_random_button.click
-
-    expect(page.first_project.assigned_reviewer)
-      .to have_text second_user.name
-    expect(page.first_project.history_table.content)
-      .to have_text user.name
-    expect(page.first_project.history_table.content)
-      .not_to have_text second_user.name
-
-    page.first_project.check_button.click
-    page.first_project.history_button.click
-
-    expect(page.first_project.history_table
-      .checks_number).to eq 2
-    expect(page.first_project.history_table.content)
-      .not_to have_text second_user.name
+    page.find(".btn", text: "Show").click
+    expect(page).to have_content user.name
   end
 end

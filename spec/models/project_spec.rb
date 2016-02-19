3ca86e13246e402cd3390b84b7034e5521ba3d48
Szymon Baranowski <szymon.baranowski@netguru.pl>
2016-02-19 08:51:14 +0100

    Bump shoulda-matchers to 3.1.1

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,9 +2,15 @@ require "rails_helper"
 
 describe Project do
   describe "ActiveModel validations" do
-    it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_presence_of(:channel_name) }
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_uniqueness_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:channel_name) }
+    it { should validate_presence_of(:name) }
+  end
+
+  # workaround for issue connected to not-null unique values
+  # https://github.com/thoughtbot/shoulda-matchers/issues/600
+  it "validates uniqueness of name" do
+    FactoryGirl.create(:project)
+    should validate_uniqueness_of(:name)
   end
 end

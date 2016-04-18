dfc16749785325c0b5c7b43010de3c071cce6624
Adam Nowak <adam.nowak@netguru.co>
2016-04-18 21:35:11 +0200

    no hash rockets here

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ Rails.application.routes.draw do
   end
 
   get "/auth/:provider/callback" => "sessions#create"
-  get "/signin" => "sessions#new", :as => :signin
-  get "/signout" => "sessions#destroy", :as => :signout
+  get "/signin" => "sessions#new", as: :signin
+  get "/signout" => "sessions#destroy", as: :signout
   get "/auth/failure" => "sessions#failure"
 end

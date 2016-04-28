1b675219be43c40b37598b51c75c78f1426a2fdf
Tomasz Pewiński <pewniak747@gmail.com>
2016-04-28 09:18:06 +0200

    added supervisor slack channel attribute

--- spec/decorators/reminder_decorator/base_spec.rb
+++ spec/decorators/reminder_decorator/base_spec.rb
@@ -19,4 +19,20 @@ describe ReminderDecorator::Base do
       end
     end
   end
+
+  describe "#supervisor_slack_channel_display" do
+    context "supervisor_slack_channel present" do
+      it "return supervisor_slack_channel" do
+        reminder.supervisor_slack_channel = "channel"
+        expect(decorator.supervisor_slack_channel_display).to eq("channel")
+      end
+    end
+
+    context "supervisor_slack_channel not present" do
+      it "return placeholder string" do
+        reminder.supervisor_slack_channel = ""
+        expect(decorator.supervisor_slack_channel_display).to eq("Not specified.")
+      end
+    end
+  end
 end

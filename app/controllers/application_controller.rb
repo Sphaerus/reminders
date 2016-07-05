bf8a9904a86ca996a37f9c69443d73c06b3d7674
Adam Nowak <adam.nowak@netguru.co>
2016-07-06 00:34:17 +0200

    update decent_exposure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-  decent_configuration do
-    strategy DecentExposure::StrongParametersStrategy
-  end
   protect_from_forgery with: :exception
 
   helper_method :current_user

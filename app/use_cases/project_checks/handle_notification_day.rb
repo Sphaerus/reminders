ff3f4201d0f7471657cde306591ed5c303f2d265
Krzysztof Kawa <krzysztofk131@gmail.com>
2017-01-10 14:31:06 +0100

    Add slack notifications for each project member

--- app/use_cases/project_checks/handle_notification_day.rb
+++ app/use_cases/project_checks/handle_notification_day.rb
@@ -3,5 +3,16 @@ module ProjectChecks
     def notification_template
       reminder.notification_text
     end
+
+    def notify!
+      project_channel.present? && ['members'].each do |member|
+        notifier
+          .send_message notification, channel: member
+      end
+    end
+
+    def project_channel
+      notifier.client.channels_list['channels'].detect { |channel| channel['name'] == check.project.channel_name }
+    end
   end
 end

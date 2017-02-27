be54919bbc9cab1a4d1886f88e84a322f390cc85
Andrzej Piątyszek <andrzej.piatyszek@netguru.pl>
2017-02-27 13:37:07 +0100

    ignore missing apple icon error

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,5 +2,12 @@ require "rollbar/rails"
 
 Rollbar.configure do |config|
   config.access_token = AppConfig.rollbar_token
+  
+  config.exception_level_filters.merge!({
+    'ActionController::RoutingError' => lambda do |error|
+      error.message.include?('apple-touch-icon') ? 'ignore' : 'error'
+    end
+  })
+  
   config.enabled = false if Rails.env.test? || Rails.env.development?
 end

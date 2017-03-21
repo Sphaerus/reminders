e17dabad8f3ce552dc120833c2dc9c04f8a54f85
Michal Musialik <michal.musialik@netguru.co>
2017-03-21 15:27:31 +0100

    Add specs for project check model

---
+++ spec/models/project_check_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe ProjectCheck do
+  it { is_expected.to belong_to :project }
+  it { is_expected.to belong_to :reminder }
+  it { is_expected.to belong_to(:last_check_user).class_name("User") }
+  it { is_expected.to have_many(:check_assignments)
+                      .order(created_at: :desc)
+                      .dependent(:destroy) }
+
+  it "validates that project is enabled before enabling self" do
+    project_check = create(:project_check,
+                           enabled: false,
+                           project: create(:project, enabled: false))
+    project_check.update(enabled: true)
+    expect(project_check).not_to be_valid
+  end
+end

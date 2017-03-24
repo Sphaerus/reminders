1b1af736de60e04aa223988b9486cc0b11ea8cab
Jacek Jakubik <jacek.jakubik@netguru.pl>
2017-03-24 15:19:30 +0100

    Add jira task to cronotab

--- config/cronotab.rb
+++ config/cronotab.rb
@@ -10,8 +10,22 @@ class CronRemindersCheckAll
   end
 end
 
+class CronRemindersCreateJiraIssues
+  def perform
+    Rake::Task["reminders:create_jira_issues"].execute
+  end
+end
+
+## 30 9 * * 1-5
 Crono.perform(CronRemindersCheckAll).every 1.week, on: :monday, at: {hour: 9, min: 30}
 Crono.perform(CronRemindersCheckAll).every 1.week, on: :tuesday, at: {hour: 9, min: 30}
 Crono.perform(CronRemindersCheckAll).every 1.week, on: :wednesday, at: {hour: 9, min: 30}
 Crono.perform(CronRemindersCheckAll).every 1.week, on: :thursday, at: {hour: 9, min: 30}
 Crono.perform(CronRemindersCheckAll).every 1.week, on: :friday, at: {hour: 9, min: 30}
+
+## 45 9 * * 1-5
+Crono.perform(CronRemindersCreateJiraIssues).every 1.week, on: :monday, at: {hour: 9, min: 45}
+Crono.perform(CronRemindersCreateJiraIssues).every 1.week, on: :tuesday, at: {hour: 9, min: 45}
+Crono.perform(CronRemindersCreateJiraIssues).every 1.week, on: :wednesday, at: {hour: 9, min: 45}
+Crono.perform(CronRemindersCreateJiraIssues).every 1.week, on: :thursday, at: {hour: 9, min: 45}
+Crono.perform(CronRemindersCreateJiraIssues).every 1.week, on: :friday, at: {hour: 9, min: 45}

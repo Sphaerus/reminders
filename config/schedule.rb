fc1d5f14eda9237e977bf607d44030907edc1f3e
Jacek Jakubik <jacek.jakubik@netguru.pl>
2017-03-24 15:00:35 +0100

    Use new configuration * Update Dockerfile * Use crono instead of whenever * Use capose (docker-compose) * Update deployment configuration

--- config/schedule.rb
+++
@@ -1,6 +0,0 @@
-# Learn more: http://github.com/javan/whenever
-
-# Monday-Friday at 9:30AM
-every "30 9 * * 1-5" do
-  rake "reminders:check_all"
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111095444) do
+ActiveRecord::Schema.define(version: 20170324135327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 20170111095444) do
     t.index ["contact_person_id"], name: "index_check_assignments_on_contact_person_id", using: :btree
     t.index ["project_check_id"], name: "index_check_assignments_on_project_check_id", using: :btree
     t.index ["user_id"], name: "index_check_assignments_on_user_id", using: :btree
+  end
+
+  create_table "crono_jobs", force: :cascade do |t|
+    t.string   "job_id",            null: false
+    t.text     "log"
+    t.datetime "last_performed_at"
+    t.boolean  "healthy"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.index ["job_id"], name: "index_crono_jobs_on_job_id", unique: true, using: :btree
   end
 
   create_table "project_checks", force: :cascade do |t|
